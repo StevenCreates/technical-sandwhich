5a3a42676673e98fb0c0974c2b3c8407fd25f870
Steven Hofheins <stevenisinsane@gmail.com>
2019-11-21 16:23:15 -0700

    make it better

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-var burger = require("../models/burgers");
+var burger = require("../models/burger");
 
 router.get("/", function(req, res)
 {
