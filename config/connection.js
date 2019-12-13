d3a61834c6441cf0b3f2e790bb7c8b09d3023608
Steven Hofheins <stevenisinsane@gmail.com>
2019-12-12 19:41:48 -0700

    finishing missing assignment

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ else
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "root",
+        password: "Monster08!!",
         database: "burgers_db"
     });
 }
