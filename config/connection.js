26952d445ab64102d0a4749d2aa9218874903be7
Steven Hofheins <stevenisinsane@gmail.com>
2019-12-12 20:14:29 -0700

    make it better

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ else
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "Monster08!!",
+        password: "root",
         database: "burgers_db"
     });
 }
