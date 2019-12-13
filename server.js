6f61588b206dc2eebc9c961c857fc094a35e0146
Steven Hofheins <stevenisinsane@gmail.com>
2019-12-12 20:39:24 -0700

    make it better

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require("express");
+
 var routes = require("./controllers/burgers_controller");
-require('dotenv').config()
+
 var PORT = process.env.PORT || 8080;
+
 var app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -12,9 +17,16 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
 app.use(routes);
 
 app.listen(PORT, function()
 {
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
+//let printStuff = function(result)
+//{
+    //console.log("-----------------------");
+    //console.log(result);
+//}
